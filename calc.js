d7d61839cbcc17adad86d1298a12a96f42f2d2a5
NepeeQ <waderan355@gmail.com>
2023-04-07 21:47:21 +0200

    Added basic functionality

--- calc.js
+++ calc.js
@@ -7,15 +7,16 @@ const equalBtn = document.querySelector("#equal");
 const screen = document.querySelector("#screenContent");
 
 
-let firstNumber = [];
-let secondNumber = [];
+let firstNumber = "";
+let secondNumber = "";
 
 let operationState = false; // false - no operation now (first digit typed), true - operation now (second digit typed)
 
+let currentOperationType = {};
 
 function updateScreen(val){
 
-    screen.textContent += val.at(-1);
+    screen.textContent = val;
  
 }
 
@@ -24,11 +25,127 @@ for (let i = 0; i < digitsBtns.length; i++) {
     digitsBtns[i].addEventListener("click", function() {
         if(!operationState)
         {
-            firstNumber.push(digitsBtns[i].textContent)
-            updateScreen(firstNumber)
+            firstNumber += digitsBtns[i].textContent;
+            updateScreen(firstNumber);
         }
         else{
-            secondNumber.push(digitsBtns[i].textContent);
+            secondNumber += digitsBtns[i].textContent;
+            updateScreen(secondNumber);
         };
     });
 }
+
+for (let i = 0; i < operationBtns.length; i++)
+{
+    operationBtns[i].addEventListener("click", function()
+    {
+        if (firstNumber.length != 0)
+        {
+            operationBtns[i].style.backgroundColor = "darkgray";
+            currentOperationType = operationBtns[i];
+            operationState = true;
+        }
+    })
+}
+
+clearBtn.addEventListener("click", clearCalc);
+
+function clearCalc()
+{
+    if (!operationState)
+    {
+        firstNumber = "";
+        screen.textContent = "";
+    }
+    else if(operationState && secondNumber != ""){
+        secondNumber = "";
+        screen.textContent = "";
+    }
+    else{
+        firstNumber = "";
+        screen.textContent = "";
+        operationState = false;
+        clearOperation();
+    }
+}
+
+delBtn.addEventListener("click", deleteChar);
+
+function deleteChar(){
+    if (!operationState)
+    {
+        firstNumber = firstNumber.slice(0, -1);
+        updateScreen(firstNumber);
+        console.log("dfasdfsad")
+    }
+    else{
+        secondNumber = secondNumber.slice(0, -1);
+        updateScreen(secondNumber);
+    }
+}
+
+
+equalBtn.addEventListener("click", evaluate)
+
+function evaluate(){
+
+    if (currentOperationType.name == "div")
+    {
+        firstNumber = divide(Number(firstNumber), Number(secondNumber));
+        updateScreen(firstNumber)
+        secondNumber = "";
+        clearOperation();
+    }
+    else if (currentOperationType.name == "multiply")
+    {
+        firstNumber = multiply(Number(firstNumber), Number(secondNumber));
+        updateScreen(firstNumber);
+        secondNumber = "";
+        clearOperation();
+    }
+    else if(currentOperationType.name == "minus")
+    {
+        firstNumber = substract(Number(firstNumber), Number(secondNumber));
+        updateScreen(firstNumber);
+        secondNumber = "";
+        clearOperation();
+    }
+    else if(currentOperationType.name == "plus")
+    {
+        firstNumber = add(Number(firstNumber), Number(secondNumber));
+        updateScreen(firstNumber);
+        secondNumber = "";
+        clearOperation();
+    }
+}
+
+function divide(num1, num2){
+    if(num2 != 0)
+    {
+        return Math.round((num1 / num2) * 100) / 100
+    }
+    else 
+    {
+        updateScreen("Can't divide by 0!")
+    }
+}
+
+function multiply(num1, num2){
+
+    return num1 * num2;
+}
+
+function substract(num1, num2)
+{
+    return num1 - num2;
+}
+ function add(num1, num2)
+ {
+    return num1 + num2;
+ }
+
+ function clearOperation()
+ {
+    currentOperationType.style.backgroundColor = "white" // change to correct
+    currentOperationType = {};
+ }
